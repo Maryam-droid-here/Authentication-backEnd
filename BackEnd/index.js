f50b6a4a83c91a9aa9f7ee2c3ea54bec69117fc5
Maryam-droid-here <maryam.130904@gmail.com>
2025-06-14 23:13:33 +0500

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -12,7 +12,11 @@ const PORT = process.env.PORT || 3000
 //Middleware????
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors()); 
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 app.use("/api/user", userRoute)
 db().then(() => {
